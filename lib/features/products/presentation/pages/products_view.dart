76a5ce46bcb076447b800dcc8446217a8334bef7
Zewin <mohammedzewin01@gmail.com>
2025-11-09 07:53:32 +0300

    fix Pagination

--- lib/features/products/presentation/pages/products_view.dart
+++ lib/features/products/presentation/pages/products_view.dart
@@ -1,3 +1,92 @@
+// // import 'package:fada_alhalij_web/core/di/di.dart';
+// // import 'package:fada_alhalij_web/core/resources/assets_manager.dart';
+// // import 'package:fada_alhalij_web/core/resources/color_manager.dart';
+// // import 'package:fada_alhalij_web/features/app_search/presentation/bloc/search_cubit.dart';
+// //
+// // import 'package:fada_alhalij_web/features/products/data/models/response/get_products_by_category_model.dart';
+// // import 'package:fada_alhalij_web/features/products/presentation/cubit/products_cubit.dart';
+// // import 'package:fada_alhalij_web/features/products/presentation/widgets/products_body.dart';
+// // import 'package:fada_alhalij_web/core/widgets/ske_grid_product.dart';
+// // import 'package:flutter/material.dart';
+// // import 'package:flutter_bloc/flutter_bloc.dart';
+// // import 'package:flutter_svg/svg.dart';
+// //
+// //
+// // import '../../../../core/resources/style_manager.dart';
+// //
+// // class ProductsView extends StatefulWidget {
+// //   const ProductsView({super.key, required this.idCategory});
+// //
+// //   final String? idCategory;
+// //
+// //   @override
+// //   State<ProductsView> createState() => _ProductsViewState();
+// // }
+// //
+// // class _ProductsViewState extends State<ProductsView> {
+// //   late ProductsCubit viewModel;
+// //
+// //   @override
+// //   void initState() {
+// //     viewModel = getIt<ProductsCubit>();
+// //     super.initState();
+// //   }
+// //
+// //   @override
+// //   void dispose() {
+// //     viewModel.close();
+// //     super.dispose();
+// //   }
+// //
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return BlocProvider.value(
+// //       value: viewModel..getProductsData(idCategory: widget.idCategory ?? ''),
+// //       child: BlocBuilder<ProductsCubit, ProductsState>(
+// //         builder: (context, state) {
+// //           if (state is ProductsSuccess) {
+// //             List<ProductsRelations> products =
+// //                 state.productsModelEntity?.productsData?.productsRelations?.reversed.toList() ??
+// //                 [];
+// //             SearchCubit.get(context).searchProducts(products.reversed.toList());
+// //             return products.isNotEmpty
+// //                 ? ProductsBody(products: products,)
+// //                 : Center(
+// //                   child: Column(
+// //                     mainAxisAlignment: MainAxisAlignment.center,
+// //                     children: [
+// //                       SizedBox(height: 24),
+// //                       Text(
+// //                         'لا يوجد منتجات',
+// //                         style: getSemiBoldStyle(
+// //                           color: ColorManager.indigoDark,
+// //                           fontSize: 20,
+// //                         ),
+// //                       ),
+// //                       SizedBox(height: 24),
+// //                       AspectRatio(
+// //                         aspectRatio: 16 / 9,
+// //                         child: SvgPicture.asset(Assets.emptyList),
+// //                       ),
+// //                     ],
+// //                   ),
+// //                 );
+// //           }
+// //
+// //           if (state is ProductsLoading) {
+// //             return SkeGridProduct();
+// //           }
+// //
+// //           return Center(
+// //             child: CircularProgressIndicator(color: ColorManager.primaryColor),
+// //           );
+// //         },
+// //       ),
+// //     );
+// //   }
+// // }
+//
+//
 // import 'package:fada_alhalij_web/core/di/di.dart';
 // import 'package:fada_alhalij_web/core/resources/assets_manager.dart';
 // import 'package:fada_alhalij_web/core/resources/color_manager.dart';
@@ -6,11 +95,9 @@
 // import 'package:fada_alhalij_web/features/products/data/models/response/get_products_by_category_model.dart';
 // import 'package:fada_alhalij_web/features/products/presentation/cubit/products_cubit.dart';
 // import 'package:fada_alhalij_web/features/products/presentation/widgets/products_body.dart';
-// import 'package:fada_alhalij_web/core/widgets/ske_grid_product.dart';
 // import 'package:flutter/material.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'package:flutter_svg/svg.dart';
-//
 //
 // import '../../../../core/resources/style_manager.dart';
 //
@@ -25,15 +112,28 @@
 //
 // class _ProductsViewState extends State<ProductsView> {
 //   late ProductsCubit viewModel;
+//   final ScrollController _scrollController = ScrollController();
 //
 //   @override
 //   void initState() {
 //     viewModel = getIt<ProductsCubit>();
+//     _setupScrollListener();
 //     super.initState();
+//   }
+//
+//   void _setupScrollListener() {
+//     _scrollController.addListener(() {
+//       // عندما يصل المستخدم لـ 80% من المحتوى
+//       if (_scrollController.position.pixels >=
+//           _scrollController.position.maxScrollExtent * 0.8) {
+//         viewModel.loadMoreProducts(); // ← يطلب المزيد من البيانات
+//       }
+//     });
 //   }
 //
 //   @override
 //   void dispose() {
+//     _scrollController.dispose();
 //     viewModel.close();
 //     super.dispose();
 //   }
@@ -44,41 +144,50 @@
 //       value: viewModel..getProductsData(idCategory: widget.idCategory ?? ''),
 //       child: BlocBuilder<ProductsCubit, ProductsState>(
 //         builder: (context, state) {
-//           if (state is ProductsSuccess) {
-//             List<ProductsRelations> products =
-//                 state.productsModelEntity?.productsData?.productsRelations?.reversed.toList() ??
-//                 [];
-//             SearchCubit.get(context).searchProducts(products.reversed.toList());
-//             return products.isNotEmpty
-//                 ? ProductsBody(products: products,)
-//                 : Center(
-//                   child: Column(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     children: [
-//                       SizedBox(height: 24),
-//                       Text(
-//                         'لا يوجد منتجات',
-//                         style: getSemiBoldStyle(
-//                           color: ColorManager.indigoDark,
-//                           fontSize: 20,
-//                         ),
-//                       ),
-//                       SizedBox(height: 24),
-//                       AspectRatio(
-//                         aspectRatio: 16 / 9,
-//                         child: SvgPicture.asset(Assets.emptyList),
-//                       ),
-//                     ],
-//                   ),
-//                 );
-//           }
+//           if (state is ProductsSuccess || state is ProductsLoadingMore) {
+//             List<ProductsRelations> products = [];
+//             bool hasMoreData = true;
 //
-//           if (state is ProductsLoading) {
-//             return SkeGridProduct();
+//             if (state is ProductsSuccess) {
+//               products = state.productsModelEntity?.productsData
+//                   ?.productsRelations ??
+//                   [];
+//               hasMoreData = state.hasMoreData;
+//             } else if (state is ProductsLoadingMore) {
+//               products = state.currentProducts;
+//             }
+//
+//             SearchCubit.get(context).searchProducts(products.reversed.toList());
+//
+//             return products.isNotEmpty
+//                 ? ProductsBody(
+//               scrollController: _scrollController,
+//               hasMoreData: hasMoreData,
+//             )
+//                 : Center(
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   SizedBox(height: 24),
+//                   Text(
+//                     'لا يوجد منتجات',
+//                     style: getSemiBoldStyle(
+//                       color: ColorManager.indigoDark,
+//                       fontSize: 20,
+//                     ),
+//                   ),
+//                   SizedBox(height: 24),
+//                   AspectRatio(
+//                     aspectRatio: 16 / 9,
+//                     child: SvgPicture.asset(Assets.emptyList),
+//                   ),
+//                 ],
+//               ),
+//             );
 //           }
 //
 //           return Center(
-//             child: CircularProgressIndicator(color: ColorManager.primaryColor),
+//             child: CircularProgressIndicator(color: ColorManager.blue),
 //           );
 //         },
 //       ),
@@ -98,7 +207,6 @@ import 'package:fada_alhalij_web/features/products/presentation/widgets/products
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
-
 import '../../../../core/resources/style_manager.dart';
 
 class ProductsView extends StatefulWidget {
@@ -112,28 +220,15 @@ class ProductsView extends StatefulWidget {
 
 class _ProductsViewState extends State<ProductsView> {
   late ProductsCubit viewModel;
-  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     viewModel = getIt<ProductsCubit>();
-    _setupScrollListener();
     super.initState();
-  }
-
-  void _setupScrollListener() {
-    _scrollController.addListener(() {
-      // عندما يصل المستخدم لـ 80% من المحتوى
-      if (_scrollController.position.pixels >=
-          _scrollController.position.maxScrollExtent * 0.8) {
-        viewModel.loadMoreProducts(); // ← يطلب المزيد من البيانات
-      }
-    });
   }
 
   @override
   void dispose() {
-    _scrollController.dispose();
     viewModel.close();
     super.dispose();
   }
@@ -144,26 +239,15 @@ class _ProductsViewState extends State<ProductsView> {
       value: viewModel..getProductsData(idCategory: widget.idCategory ?? ''),
       child: BlocBuilder<ProductsCubit, ProductsState>(
         builder: (context, state) {
-          if (state is ProductsSuccess || state is ProductsLoadingMore) {
-            List<ProductsRelations> products = [];
-            bool hasMoreData = true;
+          if (state is ProductsSuccess) {
+            List<ProductsRelations> products =
+                state.productsModelEntity?.productsData?.productsRelations ?? [];
 
-            if (state is ProductsSuccess) {
-              products = state.productsModelEntity?.productsData
-                  ?.productsRelations ??
-                  [];
-              hasMoreData = state.hasMoreData;
-            } else if (state is ProductsLoadingMore) {
-              products = state.currentProducts;
-            }
-
+            // تحديث Search products
             SearchCubit.get(context).searchProducts(products.reversed.toList());
 
             return products.isNotEmpty
-                ? ProductsBody(
-              scrollController: _scrollController,
-              hasMoreData: hasMoreData,
-            )
+                ? ProductsBody()
                 : Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -186,8 +270,36 @@ class _ProductsViewState extends State<ProductsView> {
             );
           }
 
+          if (state is ProductsFail) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.error_outline, size: 60, color: Colors.red),
+                  SizedBox(height: 16),
+                  Text(
+                    'حدث خطأ في تحميل المنتجات',
+                    style: getSemiBoldStyle(
+                      color: ColorManager.indigoDark,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () {
+                      viewModel.getProductsData(
+                        idCategory: widget.idCategory ?? '',
+                      );
+                    },
+                    child: Text('إعادة المحاولة'),
+                  ),
+                ],
+              ),
+            );
+          }
+
           return Center(
-            child: CircularProgressIndicator(color: ColorManager.blue),
+            child: CircularProgressIndicator(color: ColorManager.deepOrange),
           );
         },
       ),
--- lib/features/products/presentation/widgets/products_body.dart
+++ lib/features/products/presentation/widgets/products_body.dart
@@ -1,127 +1,82 @@
 
-// import 'package:fada_alhalij_web/core/widgets/custom_product_card.dart';
-// import 'package:fada_alhalij_web/features/app_search/presentation/bloc/search_cubit.dart';
-//
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:fada_alhalij_web/features/products/data/models/response/get_products_by_category_model.dart';
-// // class ProductsBody extends StatelessWidget {
-// //   const ProductsBody({super.key});
-// //
-// //   @override
-// //   Widget build(BuildContext context) {
-// //     return Scaffold(
-// //       body: Padding(
-// //         padding: const EdgeInsets.symmetric(horizontal: 16),
-// //         child: BlocBuilder<SearchCubit, SearchState>(
-// //           builder: (context, state) {
-// //             return LayoutBuilder(
-// //               builder: (context, constraints) {
-// //                 double mainAxisExtent = (constraints.maxWidth > 500) ? 310 : 280;
-// //                 return Padding(
-// //                   padding: EdgeInsets.only(bottom: kBottomNavigationBarHeight),
-// //                   child: GridView.builder(
-// //                     itemCount: SearchCubit.get(context).filteredProducts.length,
-// //                     physics: BouncingScrollPhysics(
-// //                       parent: AlwaysScrollableScrollPhysics(),
-// //                     ),
-// //                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-// //                       crossAxisCount: 2,
-// //                       mainAxisSpacing: 12,
-// //                       crossAxisSpacing: 12,
-// //                       childAspectRatio: .65,
-// //                       mainAxisExtent: mainAxisExtent,
-// //                       // هنا بتحكم في حجم الكرت
-// //                     ),
-// //                     itemBuilder: (context, index) => CustomProductCardWidget(
-// //                       product: SearchCubit.get(context).filteredProducts[index],
-// //                     ),
-// //                   ),
-// //                 );
-// //
-// //               },
-// //             );
-// //           },
-// //         ),
-// //       ),
-// //     );
-// //   }
-// // }
-//
-// class ProductsBody extends StatelessWidget {
-//   const ProductsBody({super.key, required this.products});
-//
-//   final List<ProductsRelations> products ;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Padding(
-//         padding: const EdgeInsets.symmetric(horizontal: 16),
-//         child: LayoutBuilder(
-//           builder: (context, constraints) {
-//             double mainAxisExtent = (constraints.maxWidth > 500) ? 310 : 280;
-//             return Padding(
-//               padding: EdgeInsets.only(bottom: kBottomNavigationBarHeight),
-//               child: GridView.builder(
-//                 itemCount: products.length,
-//                 physics: BouncingScrollPhysics(
-//                   parent: AlwaysScrollableScrollPhysics(),
-//                 ),
-//                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//                   crossAxisCount: 2,
-//                   mainAxisSpacing: 12,
-//                   crossAxisSpacing: 12,
-//                   childAspectRatio: .65,
-//                   mainAxisExtent: mainAxisExtent,
-//                   // هنا بتحكم في حجم الكرت
-//                 ),
-//                 itemBuilder: (context, index) => CustomProductCardWidget(
-//                   product: products[index],
-//                 ),
-//               ),
-//             );
-//
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 import 'package:fada_alhalij_web/core/resources/color_manager.dart';
 import 'package:fada_alhalij_web/core/widgets/custom_product_card.dart';
 import 'package:fada_alhalij_web/features/app_search/presentation/bloc/search_cubit.dart';
+import 'package:fada_alhalij_web/features/products/presentation/cubit/products_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ProductsBody extends StatelessWidget {
-  const ProductsBody({
-    super.key,
-    required this.scrollController,
-    this.hasMoreData = true,
-  });
+class ProductsBody extends StatefulWidget {
+  const ProductsBody({super.key});
 
-  final ScrollController scrollController;
-  final bool hasMoreData;
+  @override
+  State<ProductsBody> createState() => _ProductsBodyState();
+}
+
+class _ProductsBodyState extends State<ProductsBody> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  // الكشف عن الوصول لنهاية القائمة
+  void _onScroll() {
+    if (_isBottom) {
+      final cubit = context.read<ProductsCubit>();
+      cubit.loadMoreProducts();
+    }
+  }
+
+  bool get _isBottom {
+    if (!_scrollController.hasClients) return false;
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.offset;
+    // تحميل عندما نصل إلى 80% من القائمة
+    return currentScroll >= (maxScroll * 0.12);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: ClipRect(
-          child: BlocBuilder<SearchCubit, SearchState>(
-            builder: (context, state) {
-              final products = SearchCubit.get(context).filteredProducts;
+        child: BlocBuilder<SearchCubit, SearchState>(
+          builder: (context, searchState) {
+            return BlocConsumer<ProductsCubit, ProductsState>(
+              listener: (context, state) {
+                if (state is ProductsFail) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('فشل تحميل المزيد من المنتجات'),
+                      backgroundColor: Colors.red,
+                    ),
+                  );
+                }
+              },
+              builder: (context, productsState) {
+                final products = SearchCubit.get(context).filteredProducts;
+                final cubit = context.read<ProductsCubit>();
 
-              return LayoutBuilder(
+                return LayoutBuilder(
+
                   builder:(context, constraints) {
                     double mainAxisExtent = (constraints.maxWidth > 500) ? 310 : 280;
                     return Padding(
                       padding: EdgeInsets.only(bottom: kBottomNavigationBarHeight),
                       child: GridView.builder(
-                        controller: scrollController,
-                        itemCount: products.length + (hasMoreData ? 1 : 0),
+                        controller: _scrollController,
+                        itemCount: products.length + (cubit.hasMoreData ? 1 : 0),
                         physics: BouncingScrollPhysics(),
                         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount: 2,
@@ -131,34 +86,32 @@ class ProductsBody extends StatelessWidget {
                           mainAxisExtent: mainAxisExtent,
                         ),
                         itemBuilder: (context, index) {
-                          // إذا كان آخر عنصر ولدينا المزيد من البيانات، نعرض loading
-                          if (index == products.length) {
-                            return Center(
-                              child: Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: CircularProgressIndicator(
-                                  color: ColorManager.blue,
-                                ),
-                              ),
+                          // عرض المنتجات
+                          if (index < products.length) {
+                            return CustomProductCardWidget(
+                              product: products[index],
                             );
                           }
 
-                          return CustomProductCardWidget(
-                            product: products[index],
+                          // عرض loading indicator في النهاية
+                          return Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: CircularProgressIndicator(
+                                color: ColorManager.deepOrange,
+                              ),
+                            ),
                           );
                         },
                       ),
                     );
-
                   },
 
 
-
-
-
-              );
-            },
-          ),
+                );
+              },
+            );
+          },
         ),
       ),
     );
